fdb173d2ebebe058145eafa019721d507194eede
José Barrera <josebarrerag1996@gmail.com>
2023-05-04 09:41:27 -0300

    Paginación de personajes

--- src/api/characters.ts
+++ src/api/characters.ts
@@ -8,7 +8,7 @@ export const characters = {
 
     // Método para traer a todos los personajes
     // Posibilidad de utilizar la paginación para traer los resultados dependiendo de la página pasada
-    getAll: function ({ page = 1 }: { page?: number }) {
+    getAll: function ({ page }: { page?: number }) {
         return instance.get(endpoint, {
             // Parámetros
             params: {
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Grid } from "@mui/material";
+import { Box, CircularProgress, Button, Container, Grid, Pagination } from "@mui/material";
 import { HeaderComponent, CardComponent } from "../../components";
 import { characters } from "../../api/characters";
 import { TypeCharacter } from "./interface/character.interface";
@@ -7,20 +7,37 @@ import { TypeCharacter } from "./interface/character.interface";
 // Componente de tipo funcional que representará la página de inicio
 export const HomePage: React.FC<{}> = () => {
 
-  // Utilizando 'useState' para manejar el siguiente estado
-  // Empleando 'TypeCharacter[]' para manejar las propiedades de los personajes en un arreglo
-  const [allCharacters, setAllCharacters] = React.useState<TypeCharacter[] | null>(null);
+  // Utilizando 'useState' para manejar los siguientes estados
+  const [allCharacters, setAllCharacters] = React.useState<TypeCharacter[] | null>(null); // Empleando 'TypeCharacter[]' para manejar las propiedades de los personajes en un arreglo
+  const [loading, setLoading] = React.useState<boolean>(true); // Para manejar si se están cargando o no los datos de la API
+
+  // Utilizando 'useState' para manejar los estados de la paginación
+  const [page, setPage] = React.useState(1); // Para cambiar las páginas
+  const [count, setCount] = React.useState(1); // Para realizar el conteo de páginas
 
   // Utilizando 'useEffect' para lograr cargar los datos de la API
-  // Cargando los personajes de la primera página
+  // Cargando los personajes de la primera página (por defecto)
   React.useEffect(() => {
-    characters.getAll({ page: 1 }).then((r) => {
-      // console.log(r.data.results);
-      setAllCharacters(r.data.results);
-    }).catch((e) => {
-      console.error(e);
-    })
-  }, []);
+    setLoading(true);
+    characters
+      .getAll({ page })
+      .then((r) => {
+        // Obteniendo el nro total de páginas
+        setCount(r.data.info.pages);
+        // Obteniendo los personajes
+        setAllCharacters(r.data.results);
+        // Cargando los datos...
+        setTimeout(() => setLoading(false), 1000); // Duración del spinner (1 segundo)
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, [page]); // Renderizar este componente por cada vez que se cambie de página
+
+  // Método para poder actualizar el estado de 'page' y con ello, cambiar la página
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   return (
     <Container maxWidth="xl">
@@ -33,19 +50,48 @@ export const HomePage: React.FC<{}> = () => {
           </Button>
         }
       />
-      <div>
-        {
-          allCharacters?.length !== 0 ? (
-            <Grid container spacing={2} direction="row">
-              {allCharacters?.map((character) => (
-                <Grid item xs={3} key={character.id}>
-                  <CardComponent image={character.image} name={character.name} species={character.species} status={character.status} />
-                </Grid>
-              ))}
-            </Grid>
-          ) : ""
-        }
-      </div>
+      {/* Si no se han cargado los datos... */}
+      {loading ? (
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        // Si ya están cargados...
+        <>
+          <div>
+            {allCharacters!.length !== 0 ? (
+              <Grid sx={{ my: 2 }} container spacing={2} direction="row">
+                {allCharacters!.map((character) => (
+                  <Grid item xs={3} key={character.id}>
+                    <CardComponent
+                      // key={character.id}
+                      image={character.image}
+                      name={character.name}
+                      species={character.species}
+                      status={character.status}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            ) : (
+              "No data"
+            )}
+          </div>
+          <Box
+            sx={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <Pagination
+              variant="outlined"
+              color="primary"
+              count={count}
+              page={page}
+              onChange={handleChange}
+              sx={{ mb: 3 }}
+              size="large"
+            />
+          </Box>
+        </>
+      )}
     </Container>
   );
 };
